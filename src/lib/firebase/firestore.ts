b40a8c9200a3c639396e725ef8db8f83b1ec6118
Rabin Lamichhane <rabin.lc.web@gmail.com>
2025-04-30 04:26:01 +0000

    added messaging feature

--- src/lib/firebase/firestore.ts
+++ src/lib/firebase/firestore.ts
@@ -5,3 +5,4 @@ export * from './config'; // Export config like db, auth
 export * from './user'; // Export all user-related functions
 export * from './quiz'; // Export all quiz and MCQ related functions
 export * from './teacher-questions'; // Export all "Ask a Teacher" related functions
+export * from "./message"
